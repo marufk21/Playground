92b0367b34d667007b2c0471527e0d7195f13284
Maruf Khan <marufkhan2002@gmail.com>
2026-04-19 18:47:02 +0530

    Ready for deploy

--- apps/server/app/db/database.py
+++ apps/server/app/db/database.py
@@ -1,7 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./app.db"
+# Load environment variables from .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLite needs this flag for multithreaded FastAPI usage.
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
--- apps/server/app/main.py
+++ apps/server/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.user_routes import router as user_router
 from app.db.database import Base, engine
@@ -7,6 +8,19 @@ from app.db.database import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FastAPI Backend")
+
+origins = [
+    "http://localhost:5173", # dev
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 
 
